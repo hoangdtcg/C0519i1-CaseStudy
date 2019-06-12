8664b6088574197db07793bd44a18d2312b78752
dohuunghia11 <49792525+dohuunghia11@users.noreply.github.com>
2019-06-12 17:58:17 +0700

    Update Eggs.js

--- C0519I1_dohuunghia/Eggs.js
+++ C0519I1_dohuunghia/Eggs.js
@@ -6,10 +6,8 @@ let egg = function (game) {
     this.type = 1;
     this.popped = false;
     this.visible = true;
-    this.addedScore = false; // kiểm tra egg này đã được tính điểm chưa
-    this.finish = false;
-    // let self = this;
-    // khởi tạo egg
+    this.addedScore = false; 
+    
     this.init = function () {
         this.type = this.getRandomInt(1, 2);
         if (this.type === 1) {
@@ -25,33 +23,21 @@ let egg = function (game) {
     this.getRandomInt = function (min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     };
-
-    // tạo xong thì mỗi lần lặp update
+    
     this.update = function () {
-            if(!this.finish){
-                this.popped = false;
-                if (this.y <= 730) {
-                    this.y += 2;
-                    this.checkInBasket();
-                } else {
-                    this.popped = true;
-                    this.game.basket.dreaseHP(this.popped);
-                    this.visible = false;
-                    this.finish = false;
-
-                }
-            }
+        if (this.y <= this.game.canvas.height) {
+            this.y += 2;
+        }
     };
-
+    
     this.checkInBasket = function () {
         if (
             (this.x > this.game.basket.x) &&
             (this.x < (this.game.basket.x + this.game.resource.basket.img.width)) &&
             (this.y >= 700)
         ) {
-            // biến mất egg
             this.visible = false;
-            // tăng score
+
             if (this.addedScore === false) {
                 this.game.score += this.type;
                 this.addedScore = true;
@@ -59,14 +45,12 @@ let egg = function (game) {
         }
     };
 
-    // vẽ eggs
     this.draw = function () {
-        if (this.visible) {
-            this.game.context.drawImage(
-                this.img,
-                this.x - (this.img.width / 2),
-                this.y
-            )
-        }
+        this.game.context.drawImage(
+            this.img,
+            this.x - (this.img.width / 2),
+            this.y
+        )
+
     };
 };
