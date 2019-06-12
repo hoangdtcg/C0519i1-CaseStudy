157f78c80450845f252ccc5e47ba8c2b046e078a
dohuunghia11 <49792525+dohuunghia11@users.noreply.github.com>
2019-06-12 18:18:42 +0700

    Update Eggs.js

--- C0519I1_dohuunghia/Eggs.js
+++ C0519I1_dohuunghia/Eggs.js
@@ -6,8 +6,8 @@ let egg = function (game) {
     this.type = 1;
     this.popped = false;
     this.visible = true;
-    this.addedScore = false; 
-    
+    this.addedScore = false;
+
     this.init = function () {
         this.type = this.getRandomInt(1, 2);
         if (this.type === 1) {
@@ -15,7 +15,6 @@ let egg = function (game) {
         } else {
             this.img = this.game.resource.egg2.img;
         }
-
         let positions = [78, 228, 378, 528, 678, 828, 978, 1128, 1378, 1478];
         this.x = positions[this.getRandomInt(0, 8)];
     };
@@ -23,13 +22,13 @@ let egg = function (game) {
     this.getRandomInt = function (min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     };
-    
+
     this.update = function () {
         if (this.y <= this.game.canvas.height) {
             this.y += 2;
         }
     };
-    
+
     this.checkInBasket = function () {
         if (
             (this.x > this.game.basket.x) &&
